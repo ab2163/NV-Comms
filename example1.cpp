3c10922bea42546f42bbb82c74a303d9ea2f1a9d
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-07-17 17:03:08 +0100

    Added examples showing error message from compiler

---
+++ example1.cpp
@@ -0,0 +1,57 @@
+#include <thread>
+#include "stdexec/include/exec/static_thread_pool.hpp"
+#include <iostream>
+
+int main(void){
+  unsigned int num_threads = std::thread::hardware_concurrency();
+  if(num_threads == 0) {
+    std::cerr << "Unable to determine thread pool size.\n";
+    std::exit(EXIT_FAILURE);
+  }
+  exec::static_thread_pool pool(num_threads);
+  auto sched = pool.get_scheduler();
+  
+  #define ARR_SZ 10 
+
+  static int count[ARR_SZ];
+  
+  for(int ind = 0; ind < ARR_SZ; ind++){
+    count[ind] = 0;
+  }
+
+  stdexec::sender auto mysender = stdexec::schedule(sched)
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; })
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; })
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; })
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; })
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; })
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; })
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; })
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; })
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; })
+  | stdexec::bulk(stdexec::par_unseq, ARR_SZ, [&](int ind){ count[ind]++; })
+  | stdexec::then([&](){ for(int ind = 0; ind < ARR_SZ; ind++){ std::cout << count[ind] << " "; }})
+  | stdexec::then([](){ std::cout << "\n"; });
+  
+  stdexec::sync_wait(mysender);
+  
+  return 0;
+}
